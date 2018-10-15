d39c47849c3db1dd62c5a99d2ed518bd39e25721
Maarten <maartenstolk72@gmail.com>
2018-10-15 16:46:23 +0200

    styled the actor detail page

--- resources/views/actors/show.blade.php
+++ resources/views/actors/show.blade.php
@@ -8,7 +8,26 @@
     </div>
 
     <div class="content">
-        <h1 class="centered">{{ $actor->name }}</h1>
+
+        <div class="row actor">
+
+                <div class="col">
+                    <div class="actor-content">
+                        <h1 class="centered">{{ $actor->name }}</h1>
+                        <p>{{ $actor->biography }}</p>
+                    </div>
+                </div>
+
+                <div class="col-4">
+                    <div class="actor-sidebar">
+                        <img src="{{ $actor->image }}" class="img-fluid" alt="Responsive image">
+                        <div class="sidebar-inner">
+                            <p>Born in: {{ $actor->birth_place }}</p>
+                            <p>Age: {{ $actor->birth_date }}</p>
+                        </div>
+                    </div>
+                </div>
+        </div>
 
     </div>
 
