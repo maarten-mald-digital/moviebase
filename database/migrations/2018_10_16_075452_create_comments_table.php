ca783d7ec8008d1ee0d04ff81a3a3de46a12a7e7
Maarten <maartenstolk72@gmail.com>
2018-10-18 11:01:03 +0200

    fixed php artisan migrate:refresh bug

--- database/migrations/2018_10_16_075452_create_comments_table.php
+++ database/migrations/2018_10_16_075452_create_comments_table.php
@@ -30,6 +30,6 @@ class CreateCommentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('comment');
+        Schema::dropIfExists('comments');
     }
 }
